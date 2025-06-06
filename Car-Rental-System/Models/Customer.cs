91717c33b420876da0457141c146db4037477136
john-michaelg135 <johnmichaelngarcia@iskolarngbayan.pup.edu.ph>
2025-06-06 20:12:43 +0800

    customer cs update

--- Car-Rental-System/Models/Customer.cs
+++ Car-Rental-System/Models/Customer.cs
@@ -6,14 +6,33 @@ namespace Car_Rental_System.Models
     public class Customer
     {
         public int CustomerId { get; set; }
-        public string CustomerName { get; set; }
-        public string Contact { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? MiddleInnitial { get; set; }
+        public string ContactNumber { get; set; }
+        public string LicenseNumber { get; set; }
+        public string Email { get; set; }
 
-        public Customer(int customerId, string customerName, string contact)
+        public string ZipCode { get; set; }
+        public string Region { get; set; }
+        public string City { get; set; }
+        public string Barangay { get; set; }
+
+        public Customer(int customerId, string firstName, string lastName, string middleInnitial, string contactNumber, string licenseNumber, string email, string zipCode, string region, string city, string barangay)
         {
             CustomerId = customerId;
-            CustomerName = customerName;
-            Contact = contact;
+            FirstName = firstName;
+            LastName = lastName;
+            MiddleInnitial = middleInnitial;
+            ContactNumber = contactNumber;
+            LicenseNumber = licenseNumber;
+            Email = email;
+
+            ZipCode = zipCode;
+            Region = region;
+            City = city;
+            Barangay = barangay;
+
         }
 
         public static void CustomerTest()
@@ -21,9 +40,9 @@ namespace Car_Rental_System.Models
             Console.WriteLine("Customer.cs is used successfully!!!");
         }
 
-        public void CustomerInfo()
-        {
-            Console.WriteLine($"CustomerId: {CustomerId}, Name: {CustomerName}, Contact: {Contact}");
-        }
+        //public void CustomerInfo()
+        //{
+        //    Console.WriteLine($"CustomerId: {CustomerId}, Name: {CustomerName}, Contact: {Contact}");
+        //}
     }
 }
